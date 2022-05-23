1b3fed80489ac8970b2d04afb7ade3767c9a65a1
Meyers Arlean <marlean@oa-f3.msk.21-school.ru>
2022-05-23 17:31:31 +0300

    upd

---
+++ my_minishell/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marlean <marlean@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 15:10:55 by marlean           #+#    #+#             */
+/*   Updated: 2022/05/23 15:11:42 by marlean          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <stdio.h>
+
+
+
+#endif
